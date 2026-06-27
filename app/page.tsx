a7cc5e6f42aba392bc919d8b559da16f7eb3cff5
Emirhan <emirhanerkn@hotmail.com>
2026-06-27 14:48:24 +0300

    feat: add static metadata and use opengraph

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
 import {GetAlbums} from "@/components/Albums";
 import Posts from "@/components/Posts";
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: "Main Page",
+    description: "Main Page description",
+    openGraph: {
+        title: "OpenGraph",
+        description: "OpenGraph",
+        url: "https://opengraph.org/",
+        images: ['/og.jpg'],
+    }
+}
 
 export default function Home() {
-
   return (
      <div>
          <Posts/>
